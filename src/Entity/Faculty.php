d1e12aed6fe21ada9aa879a83737ed1a3b3f0db7
Boris Onyshchenko <oboris@ukr.net>
2020-02-24 17:19:36 +0200

    added faculty entity

---
+++ src/Entity/Faculty.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\FacultyRepository")
+ */
+class Faculty
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $shortName;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Auditorium", mappedBy="faculty")
+     */
+    private $auditoriums;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Group", mappedBy="faculty")
+     */
+    private $groups;
+
+    public function __construct()
+    {
+        $this->auditoriums = new ArrayCollection();
+        $this->groups = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getShortName(): ?string
+    {
+        return $this->shortName;
+    }
+
+    public function setShortName(string $shortName): self
+    {
+        $this->shortName = $shortName;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Auditorium[]
+     */
+    public function getAuditoriums(): Collection
+    {
+        return $this->auditoriums;
+    }
+
+    public function addAuditorium(Auditorium $auditorium): self
+    {
+        if (!$this->auditoriums->contains($auditorium)) {
+            $this->auditoriums[] = $auditorium;
+            $auditorium->setFaculty($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAuditorium(Auditorium $auditorium): self
+    {
+        if ($this->auditoriums->contains($auditorium)) {
+            $this->auditoriums->removeElement($auditorium);
+            // set the owning side to null (unless already changed)
+            if ($auditorium->getFaculty() === $this) {
+                $auditorium->setFaculty(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Group[]
+     */
+    public function getGroups(): Collection
+    {
+        return $this->groups;
+    }
+
+    public function addGroup(Group $group): self
+    {
+        if (!$this->groups->contains($group)) {
+            $this->groups[] = $group;
+            $group->setFaculty($this);
+        }
+
+        return $this;
+    }
+
+    public function removeGroup(Group $group): self
+    {
+        if ($this->groups->contains($group)) {
+            $this->groups->removeElement($group);
+            // set the owning side to null (unless already changed)
+            if ($group->getFaculty() === $this) {
+                $group->setFaculty(null);
+            }
+        }
+
+        return $this;
+    }
+}
